998cfb7e5c6485a89e0f8ba92fed441c13df15aa
AD <burningggflamesss@gmail.com>
2026-06-09 11:04:26 +0545

    Add react-icons dependency and enhance BottomBar component with date and time display

--- src/components/BottomBar.tsx
+++ src/components/BottomBar.tsx
@@ -1,9 +1,36 @@
+import { getDateTime } from "#/lib/utils.ts";
+import { useEffect, useState } from "react";
+import { FaBatteryThreeQuarters, FaWifi } from "react-icons/fa6";
+import { HiSpeakerWave } from "react-icons/hi2";
+
 function BottomBar() {
+    const [dateTimeData, setDateTimeData] = useState(() => getDateTime())
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setDateTimeData(() => getDateTime())
+        }, 60_000);
+
+        return () => clearInterval(timer)
+    }, [])
+
 	return (
-        <footer className="glassmorphism h-14 w-[80dvw] flex flex-row items-center justify-between p-2 absolute bottom-2 left-1/2 -translate-x-1/2 rounded-xl">
-            FRO
-        </footer>
-    )
+		<footer className="glassmorphism h-16 w-[80dvw] flex flex-row items-center justify-between p-2 absolute bottom-2 left-1/2 -translate-x-1/2 rounded-xl">
+			<section></section>
+			<section></section>
+			<section className="h-full flex flex-row gap-4 items-center justify-center">
+				<div className="stats-panel flex flex-row items-center justify-center gap-2">
+					<FaWifi />
+					<HiSpeakerWave />
+					<FaBatteryThreeQuarters />
+				</div>
+                <div className="date-panel flex flex-col items-center justify-start">
+                    <p>{dateTimeData.time}</p>
+                    <p>{dateTimeData.date}</p>
+                </div>
+			</section>
+		</footer>
+	);
 }
 
 export default BottomBar;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,16 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getDateTime() {
+  const now = new Date();
+
+  return {
+    time: now.toLocaleTimeString("en-GB", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
+    }),
+    date: now.toLocaleDateString("en-GB"),
+  };
+}
