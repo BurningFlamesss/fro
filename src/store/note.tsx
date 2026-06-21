74c6aae80a6df31e9627486fd1820a1c43af9e73
AD <burningggflamesss@gmail.com>
2026-06-21 10:27:15 +0545

    refactor: define NoteTab and NoteStore interfaces for improved type safety in Zustand store

--- src/store/note.tsx
+++ src/store/note.tsx
@@ -2,9 +2,30 @@ import { create } from "zustand";
 import { persist } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
-export const useNoteStore = create(
+interface NoteTab { 
+    id: string;
+    title: string;
+    content: string;
+}
+
+interface NoteStore {
+    tabs: Array<NoteTab>;
+    activeId: string;
+
+}
+
+export const useNoteStore = create<NoteStore>()(
 	persist(
-		immer((set, get) => ({})),
+		immer((set, get) => ({
+            tabs: [{
+                id: crypto.randomUUID(),
+                title: "Untitled",
+                content: ""
+            }],
+            activeId: get().tabs[0].id,
+
+            
+        })),
 		{
 			name: "frotes-storage",
 		},
