b8d76f9b32481485cd5a47b7fe761820d439d4c7
AD <burningggflamesss@gmail.com>
2026-06-24 20:21:42 +0545

    enhance Froame component: implement layout and styling for the Froame Launcher interface

--- src/apps/Froame.tsx
+++ src/apps/Froame.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { cn } from "#/lib/utils.ts";
 
 function Froame() {
-  return (
-    <div>Froame</div>
-  )
+	return (
+		<div className="p-4">
+			<h2 className="mb-4 text-lg font-semibold">Froame Launcher</h2>
+			<div className="grid grid-cols-2 gap-3 lg:grid-cols-6">
+				<div className="flex flex-col items-center justify-center gap-y-1">
+					<button
+						type="button"
+						className={cn(
+							"group relative overflow-hidden rounded-xl transition-all cursor-pointer",
+						)}
+					>
+						<img
+							src={"/public/apps/Game.svg"}
+							alt={""}
+							loading="lazy"
+							className="h-full w-full object-cover"
+						/>
+					</button>
+					<div className="">
+						<p className="truncate text-xs font-medium text-background">
+							Froame
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Froame
+export default Froame;
