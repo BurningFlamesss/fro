41b87c7112f805f4fbe1831b2def7f33ce1468a7
AD <burningggflamesss@gmail.com>
2026-06-25 07:59:02 +0545

    enhance Froculator component: refactor button handling with CalculatorButton interface and update button rendering logic

--- src/apps/Froculator.tsx
+++ src/apps/Froculator.tsx
@@ -1,4 +1,69 @@
-import React from "react";
+type ButtonType = "number" | "operator" | "equals" | "function" | "clear";
+
+interface CalculatorButton {
+	label: string;
+	value: string;
+	type: ButtonType;
+	span?: boolean;
+}
+
+const buttons: Array<CalculatorButton> = [
+	{
+		label: "1",
+		value: "1",
+		type: "number",
+	},
+	{
+		label: "2",
+		value: "2",
+		type: "number",
+	},
+	{
+		label: "3",
+		value: "3",
+		type: "number",
+	},
+	{
+		label: "4",
+		value: "4",
+		type: "number",
+	},
+	{
+		label: "5",
+		value: "5",
+		type: "number",
+	},
+	{
+		label: "6",
+		value: "6",
+		type: "number",
+	},
+	{
+		label: "7",
+		value: "7",
+		type: "number",
+	},
+	{
+		label: "8",
+		value: "8",
+		type: "number",
+	},
+	{
+		label: "9",
+		value: "9",
+		type: "number",
+	},
+	{
+		label: "0",
+		value: "0",
+		type: "number",
+	},
+	{
+		label: "=",
+		value: "=",
+		type: "equals",
+	},
+];
 
 function Froculator() {
 	return (
@@ -9,13 +74,13 @@ function Froculator() {
 			</div>
 
 			<div className="grid grid-cols-8 gap-1.5 flex-1">
-				{Array.from({ length: 48 }).map((element, index) => (
+				{buttons.map((element, index) => (
 					<button
 						key={`calculator-button-${element}-${index}`}
 						type="button"
 						className="rounded-md font-medium text-sm flex justify-center items-center p-1"
 					>
-						KEYS
+						{element.label}
 					</button>
 				))}
 			</div>
