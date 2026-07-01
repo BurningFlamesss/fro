c4bd59ff9dbda90062b5007853ceac6aaba50ffc
AD <burningggflamesss@gmail.com>
2026-07-01 08:19:51 +0545

    feat: refactor Froxplorer component layout and add breadcrumb navigation

--- src/apps/Froxplorer.tsx
+++ src/apps/Froxplorer.tsx
@@ -1,7 +1,7 @@
-import React from "react";
 import { useFileSystemStore } from "#/store/fs.tsx";
 import { useWindowStore } from "#/store/window.tsx";
 import type { WindowInstance } from "../constants";
+import { FaArrowUp } from "react-icons/fa6";
 
 function Froxplorer({ windowId }: { windowId: WindowInstance["id"] }) {
 	const win = useWindowStore((state) => state.windows[windowId]);
@@ -18,9 +18,17 @@ function Froxplorer({ windowId }: { windowId: WindowInstance["id"] }) {
 		rootId,
 	} = useFileSystemStore();
 
+	const breadcrumb = ""
+
 	return (
-		<div>
-			<h1>Hello</h1>
+		<div className="flex flex-col h-full">
+			<div className="flex items-center p-2 gap-2 border-b">
+				<button type="button" className="p-1 rounded-full hover:bg-background/10 cursor-pointer">
+					<FaArrowUp />
+				</button>
+				<span className="text-sm opacity-80">{breadcrumb}</span>
+			</div>
+			
 		</div>
 	);
 }
