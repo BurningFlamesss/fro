b1e0835b72e0d25215806462d45c5e2018b52ed7
AD <burningggflamesss@gmail.com>
2026-07-02 07:36:13 +0545

    feat: reorganize imports and enhance breadcrumb logic in Froxplorer component

--- src/apps/Froxplorer.tsx
+++ src/apps/Froxplorer.tsx
@@ -1,7 +1,8 @@
+import { useState } from "react";
+import { FaArrowUp } from "react-icons/fa6";
 import { useFileSystemStore } from "#/store/fs.tsx";
 import { useWindowStore } from "#/store/window.tsx";
 import type { WindowInstance } from "../constants";
-import { FaArrowUp } from "react-icons/fa6";
 
 function Froxplorer({ windowId }: { windowId: WindowInstance["id"] }) {
 	const win = useWindowStore((state) => state.windows[windowId]);
@@ -16,19 +17,31 @@ function Froxplorer({ windowId }: { windowId: WindowInstance["id"] }) {
 		removeFromDesktop,
 		renameNode,
 		rootId,
+		getChildren,
+		getPath,
 	} = useFileSystemStore();
 
-	const breadcrumb = ""
+	const [currentFolderId, setCurrentFolderId] = useState<string>(folderId);
+	const [renameTarget, setRenameTarget] = useState<string | null>(null);
+	const [newName, setNewName] = useState<string>("");
+
+	const children = getChildren(currentFolderId);
+	const currentFolder = nodes[currentFolderId];
+
+	const pathIds = getPath(currentFolderId);
+	const breadcrumb = pathIds?.map((id) => nodes[id].name).join(" / ");
 
 	return (
 		<div className="flex flex-col h-full">
 			<div className="flex items-center p-2 gap-2 border-b">
-				<button type="button" className="p-1 rounded-full hover:bg-background/10 cursor-pointer">
+				<button
+					type="button"
+					className="p-1 rounded-full hover:bg-background/10 cursor-pointer"
+				>
 					<FaArrowUp />
 				</button>
 				<span className="text-sm opacity-80">{breadcrumb}</span>
 			</div>
-			
 		</div>
 	);
 }
