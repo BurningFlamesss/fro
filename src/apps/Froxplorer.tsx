2b2cc878155361af2d23b84ffb486c9586712462
AD <burningggflamesss@gmail.com>
2026-07-08 08:43:43 +0545

    feat: integrate react-dropzone for file upload functionality in Froxplorer component

--- src/apps/Froxplorer.tsx
+++ src/apps/Froxplorer.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Dropzone from "react-dropzone";
 import { FaArrowUp } from "react-icons/fa6";
 import {
 	ContextMenu,
@@ -11,10 +12,11 @@ import {
 	searchFileAssociatesThroughExtension,
 } from "#/lib/utils.ts";
 import { type FileNode, useFileSystemStore } from "#/store/fs.tsx";
+import { useLauncherStore } from "#/store/launcher.tsx";
 import { useNoteStore } from "#/store/note.tsx";
 import { useWindowStore } from "#/store/window.tsx";
 import { Apps, type WindowInstance } from "../constants";
-import { useLauncherStore } from "#/store/launcher.tsx";
+import { useDrag } from "react-dnd";
 
 function Froxplorer({ windowId }: { windowId: string }) {
 	const { windows } = useWindowStore();
@@ -183,69 +185,97 @@ function Froxplorer({ windowId }: { windowId: string }) {
 				</div>
 			</div>
 
-			<div className="flex-1 overflow-auto p-2">
-				<div className="grid grid-cols-6 p-4 gap-2">
-					{children?.map((node) => (
-						<ContextMenu key={`fs-node-${node.id}`}>
-							<ContextMenuTrigger>
-								<button
-									title={node.name}
-									type="button"
-									onDoubleClick={() => handleOpen(node)}
-									className="flex flex-col items-center p-2 rounded-xl w-full cursor-pointer"
-								>
-									<img
-										className="w-12 h-12 object-contain select-none"
-										src={
-											node.type === "folder"
-												? "/general/fs/Folder.svg"
-												: `/general/fs/File-${searchFileAssociatesThroughExtension(parseFileName(node.name).extension).file_image}.svg`
-										}
-										alt=""
-									/>
-									{renameTarget && renameTarget.id === node.id ? (
-										<input
-											autoFocus={true}
-											value={newName}
-											onDoubleClick={(e) => e.stopPropagation()}
-											onChange={(e) => setNewName(e.target.value)}
-											onBlur={handleRenameSubmit}
-											onKeyDown={(e) =>
-												e.key === "Enter" && handleRenameSubmit()
-											}
-											className="mt-1 text-xs text-center bg-white text-black outline-none w-18"
-											type="text"
-										/>
-									) : (
-										<p className="text-xs text-center mt-1 truncate w-18 select-none">
-											{node.name}
-										</p>
-									)}
-								</button>
-							</ContextMenuTrigger>
-							<ContextMenuContent className="z-100000002">
-								<ContextMenuItem onClick={() => handleOpen(node)}>
-									Open
-								</ContextMenuItem>
-								<ContextMenuItem onClick={() => handleRenameStart(node)}>
-									Rename
-								</ContextMenuItem>
-								<ContextMenuItem onClick={() => addToDesktop(node.id)}>
-									Add to desktop
-								</ContextMenuItem>
-								<ContextMenuItem onClick={() => removeFromDesktop(node.id)}>
-									Remove from desktop
-								</ContextMenuItem>
-								<ContextMenuItem onClick={() => deleteNode(node.id)}>
-									Delete
-								</ContextMenuItem>
-							</ContextMenuContent>
-						</ContextMenu>
-					))}
-				</div>
-			</div>
+			<Dropzone onDrop={() => {}}>
+				{({ getRootProps, getInputProps }) => {
+					return (
+						<div {...getRootProps()} className="flex-1 overflow-auto p-2">
+							<input {...getInputProps()} />
+							<div className="grid grid-cols-6 p-4 gap-2">
+								{children?.map((node) => (
+									<ContextMenu key={`fs-node-${node.id}`}>
+										<ContextMenuTrigger>
+											<button
+												title={node.name}
+												type="button"
+												onDoubleClick={() => handleOpen(node)}
+												className="flex flex-col items-center p-2 rounded-xl w-full cursor-pointer"
+											>
+												<img
+													className="w-12 h-12 object-contain select-none"
+													src={
+														node.type === "folder"
+															? "/general/fs/Folder.svg"
+															: `/general/fs/File-${searchFileAssociatesThroughExtension(parseFileName(node.name).extension).file_image}.svg`
+													}
+													alt=""
+												/>
+												{renameTarget && renameTarget.id === node.id ? (
+													<input
+														autoFocus={true}
+														value={newName}
+														onDoubleClick={(e) => e.stopPropagation()}
+														onChange={(e) => setNewName(e.target.value)}
+														onBlur={handleRenameSubmit}
+														onKeyDown={(e) =>
+															e.key === "Enter" && handleRenameSubmit()
+														}
+														className="mt-1 text-xs text-center bg-white text-black outline-none w-18"
+														type="text"
+													/>
+												) : (
+													<p className="text-xs text-center mt-1 truncate w-18 select-none">
+														{node.name}
+													</p>
+												)}
+											</button>
+										</ContextMenuTrigger>
+										<ContextMenuContent className="z-100000002">
+											<ContextMenuItem onClick={() => handleOpen(node)}>
+												Open
+											</ContextMenuItem>
+											<ContextMenuItem onClick={() => handleRenameStart(node)}>
+												Rename
+											</ContextMenuItem>
+											<ContextMenuItem onClick={() => addToDesktop(node.id)}>
+												Add to desktop
+											</ContextMenuItem>
+											<ContextMenuItem
+												onClick={() => removeFromDesktop(node.id)}
+											>
+												Remove from desktop
+											</ContextMenuItem>
+											<ContextMenuItem onClick={() => deleteNode(node.id)}>
+												Delete
+											</ContextMenuItem>
+										</ContextMenuContent>
+									</ContextMenu>
+								))}
+							</div>
+						</div>
+					);
+				}}
+			</Dropzone>
 		</div>
 	);
 }
 
 export default Froxplorer;
+
+const DraggableItem = ({
+	node,
+	children,
+}: {
+	node: FileNode;
+	children: React.ReactNode;
+}) => {
+	const [{ isDragging }, drag] = useDrag(() => ({
+		type: "FS_NODE",
+		item: { id: node.id },
+		collect: (monitor) => ({ isDragging: monitor.isDragging() }),
+	}));
+	return (
+		<div ref={drag} style={{ opacity: isDragging ? 0.8 : 1 }}>
+			{children}
+		</div>
+	);
+};
