315ee925314a24d958c58c9038fc213420e721c9
AD <burningggflamesss@gmail.com>
2026-07-20 22:41:41 +0545

    fix: handle empty task list in Task component to display a message when no tasks are available

--- src/widgets/Task.tsx
+++ src/widgets/Task.tsx
@@ -9,11 +9,11 @@ function Task() {
 	});
 	return (
 		<ul className="p-4 min-h-full w-full glassmorphism">
-			{tasks.map((tab, index) => (
+			{tasks.length > 0 ? tasks.map((tab, index) => (
 				<li key={`widget-task-pending-to-do-${tab[0]}`}>
 					{index + 1}. {tab[1].content}
 				</li>
-			))}
+			)) : <li className="h-full w-full flex flex-row items-center justify-center">No Any Tasks</li>}
 		</ul>
 	);
 }
