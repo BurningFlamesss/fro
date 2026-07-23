ca05460f09531bde1877a210bb80e88260672fec
AD <burningggflamesss@gmail.com>
2026-07-23 22:05:10 +0545

    feat: add cheer and break messages, and initialize audio for Pomodoro widget

--- src/widgets/Pomodoro.tsx
+++ src/widgets/Pomodoro.tsx
@@ -1,8 +1,29 @@
 import { useState } from "react";
 
+// import frokSound from "../../public/widgets/pomodoro/frok.mp3"
+
+const cheerMessages = [
+    "You can do it!",
+    "I believe in you",
+    "You're amazing",
+    "Keep going",
+    "Stay Frokused"
+]
+
+const breakMessages = [
+    "Stay Hydrated!",
+    "Eat some frogs, maybe?",
+    "Stretch you arms",
+    "Touch some grass",
+    "Walk 100 meters"
+]
+
 function Pomodoro() {
 	const [timeLeft, setTimeLeft] = useState<number>(25 * 60);
 	const [isRunning, setIsRunning] = useState<boolean>(false);
+    const [isBreak, setIsBreak] = useState<boolean>(false);
+    const frokAudio = new Audio("../../public/widgets/pomodoro/frok.mp3")
+    
 
 	return (
 		<div>
